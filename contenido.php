92d069aca6609ca776eac95c4b8826bca850cb65
Alberto <beto.ska@hotmail.com>
2020-10-02 16:20:08 -0500

    primer commit

---
+++ contenido.php
@@ -0,0 +1,28 @@
+<?php 
+
+	function contenido(){
+		$datos=array();
+
+		$datos[0]="img/muay.jpg"."||".
+				  "Muay Thai"."||".
+				  "conocido también como boxeo tailandés, o tradicionalmente como el arte de las ocho extremidades es un deporte de contacto de los más peligrosos que hay y por lo tanto se le considera un deporte de contacto extremo e ilegal en varios países del mundo. Tiene como base el boxeo tailandés, el cual se desarrolla de pie por medio de golpes con técnicas combinadas de puños, pies, rodillas y codos, además de algunos barridos, sujeciones (para golpear) y lanzamientos.";
+
+		$datos[1]="img/box.jpg"."||".
+				  "Boxeo"."||".
+				  "es un deporte de contacto en el que dos contrincantes luchan utilizando únicamente sus puños con guantes, golpeando a su adversario de la cintura hacia arriba, dentro de un cuadrilátero especialmente diseñado para tal fin; la pelea se lleva a cabo en breves secuencias de lucha denominadas asaltos y de acuerdo a un preciso reglamento, el cual regula categorías de pesos y duración del encuentro, entre otros aspectos.";
+
+		$datos[2]="img/Jiu-jitsu.jpg"."||".
+				  "Jiu-jitsu"."||".
+				  "El ju-jutsu es un arte marcial japonés clásico o koryū budō que abarca una variedad amplia de sistemas de combate modernos basados en la defensa sin armas de uno o más agresores tanto armados como desarmados";
+
+		$datos[3]="img/taekwondo.jpg"."||".
+				  "Taekwondo"."||".
+				  "Sistema de combate sin armas, de origen coreano, hoy principalmente deporte, en que dos contendientes utilizan golpes secos dados con los puños y con los pies y en el que se han desarrollado las técnicas de salto; los combates constan de tres períodos de dos minutos cada uno.";
+
+		$datos[4]="img/Kickboxing.jpg"."||".
+				  "Kickboxing"."||".
+				  "El kickboxing es un deporte de combate de origen japonés y con desarrollo en occidente, en el cual se mezclan las técnicas del boxeo con las técnicas de patadas de algunas artes marciales como el karate y el muay thai, estando así relacionado con las artes marciales orientales.";
+		return $datos;
+	}
+
+ ?>
